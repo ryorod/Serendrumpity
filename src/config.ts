b7c1ae11c3d58df4dea66b1cb860f10258a8c492
ryorod <ryo3ishi28@gmail.com>
2023-01-12 10:13:11 +0900

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
+import { ChainOrRpc } from "@thirdweb-dev/sdk"
+
 type Env = 'development' | 'production'
 
 export const ENV: Env = 'development'
+
+export const MAINNET_NAME: ChainOrRpc = 'avalanche'
+export const TESTNET_NAME: ChainOrRpc = 'avalanche-fuji'
